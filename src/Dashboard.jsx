bec1ccdd815f7bef8f4d256309b79eb8ea309bdf
deja.craft <dejacraft28@gmail.com>
2023-04-14 23:11:25 -0400

    created dashboard function with calender

---
+++ src/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import New from './teacher_comp/New.jsx'
+import Calender from './Calender.jsx'
+
+
+function Dashboard (props){
+ 
+    return (
+    <div>I AM DASHBOARD
+    <New/>
+    <Calender/>
+    </div>
+  )}
+
+
+
+export default Dashboard;
